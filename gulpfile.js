72646ab5a0d73779a7a9a128a9e0c76f0ff6dd48
3elik <3ellik@gmail.com>
2017-02-14 18:59:54 +0400

    gulp initialized

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+global.$ = {
+  package: require('./package.json'),
+  config: require('./gulp/config'),
+  path: {
+    task: require('./gulp/paths/tasks.js'),
+    jsFoundation: require('./gulp/paths/js.foundation.js'),
+    cssFoundation: require('./gulp/paths/css.foundation.js'),
+    app: require('./gulp/paths/app.js')
+  },
+  gulp: require('gulp'),
+  del: require('del'),
+  browserSync: require('browser-sync').create(),
+  gp: require('gulp-load-plugins')(),
+  merge: require('merge-stream')
+};
+
+$.path.task.forEach(function(taskPath) {
+  require(taskPath)();
+});
+
+$.gulp.task('default', $.gulp.series(
+  'clean',
+  'sprite:img',
+  $.gulp.parallel(
+    'sass',
+    'pug',
+    'js:foundation',
+    'js:process',
+    'copy:image',
+    'copy:fonts',
+    'css:foundation',
+    'sprite:svg'
+  ),
+  $.gulp.parallel(
+    'watch',
+    'serve'
+  )
+));
